4fed171553c8c643ebca05d226fac4a1dc5efd1d
Keshav <manudeepaksharma2012@gmail.com>
2026-08-20 21:59:58 +0530

    chore: update run script configuration for school system environment

--- run.py
+++ run.py
@@ -9,4 +9,7 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    # यह लाइन रेंडर के पोर्ट को अपने आप पहचान लेगी
+    port = int(os.environ.get("PORT", 8000))
+    # host को '0.0.0.0' रखने से यह इंटरनेट पर लाइव होने के लिए तैयार हो जाता है
+    app.run(debug=False, host='0.0.0.0', port=port)
